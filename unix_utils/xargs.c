f1ceebfec39a41eae3b363ea5329f7d179262a95
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2022-11-09 16:57:08 +0000

    xargs

---
+++ unix_utils/xargs.c
@@ -0,0 +1,53 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+/*
+ *
+ * Implementation of xargs
+ * 
+ * calls exec(*char, *args[]);
+ *
+ * when using exec *args[] must have NULL as last arg
+ * args[0] is the name of the program being executed
+ *
+ * fork() returns 0 in child j
+ *
+ */
+
+int
+main (int argc, char *argv[])
+{
+    char *p;
+    char buffer[512];
+    char *args[MAXARG];
+
+    p = buffer;
+
+    int i = 0;
+    for(; i < argc - 1; i++)
+    {
+        args[i] = argv[i + 1];
+    }
+
+    args[i] = p;
+    i++;
+    while(read(0, p, 1) > 0){
+        if(*p == '\n')
+        {
+            *p++ = 0;
+            args[i] = 0;
+
+            if(fork() == 0)
+            {
+                exec(argv[1], args);
+                exit(0);
+            }else{
+                wait(0);
+            }
+        }
+        p++;
+    }
+    exit(0);
+}
